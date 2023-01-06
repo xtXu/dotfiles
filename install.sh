4e25a332e8609d6b1a48b82e1f9e91b7a11f6195
XiaotianXu <xxt993018@outlook.com>
2023-01-06 22:17:52 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo ln -s ./alacritty ~/.config/alacritty
+sudo ln -s ./kitty ~/.config/kitty
+sudo ln -s ./nvim ~/.config/nvim
+sudo ln -s ./bash/.bashrc ~/.bashrc
+sudo ln -s ./zsh/.zshrc ~/.zshrc
