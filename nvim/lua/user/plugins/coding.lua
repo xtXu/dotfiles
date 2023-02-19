0984af11fe55c8fe4cfc5041ac485ed1c24985d4
XiaotianXu <xxt990318@outlook.com>
2023-02-19 23:12:17 +0800

    neoclip

--- nvim/lua/user/plugins/coding.lua
+++ nvim/lua/user/plugins/coding.lua
@@ -282,6 +282,47 @@ return {
     end,
     -- Uncomment next line if you want to follow only stable versions
     -- version = "*" 
+	},
+
+	-- neoclip
+	{
+		"AckslD/nvim-neoclip.lua",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local function is_whitespace(line)
+				return vim.fn.match(line, [[^\s*$]]) ~= -1
+			end
+
+			local function all(tbl, check)
+				for _, entry in ipairs(tbl) do
+					if not check(entry) then
+						return false
+					end
+				end
+				return true
+			end
+
+			require("neoclip").setup({
+				preview = false;
+				keys = {
+					fzf = {
+						select = 'default',
+						paste = 'ctrl-p',
+						paste_behind = 'ctrl-P',
+						custom = {},
+					},
+				},
+
+				filter = function(data)
+					return not all(data.event.regcontents, is_whitespace)
+				end,
+			})
+
+			local opts = { noremap = true, silent = true }
+			vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>lua require('neoclip.fzf')()<cr>", opts)
+
+		end,
+
 	}
 
 }
