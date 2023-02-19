980cdaf6289f3f0d30f0b300b93e1a5fe7bdf04d
XiaotianXu <xxt990318@outlook.com>
2023-02-19 20:13:42 +0800

    mason start at first, and use mason-tool-installer to auto install the lsp with specific version

--- nvim/lua/user/plugins/lsp/init.lua
+++ nvim/lua/user/plugins/lsp/init.lua
@@ -1,18 +1,67 @@
 return {
 
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"p00f/clangd_extensions.nvim",
+	{ 
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"p00f/clangd_extensions.nvim",
+		},
+
+		config = function()
+			require("user.plugins.lsp.mason")
+			require("user.plugins.lsp.lspconfig")
+			require("user.plugins.lsp.handlers").setup()
+		end
 	},
 
-	config = function()
-		require("user.plugins.lsp.mason")
-		require("user.plugins.lsp.lspconfig")
-		require("user.plugins.lsp.handlers").setup()
-	end
+	{
+		"williamboman/mason.nvim",
+		-- event = {"VeryLazy"},
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function ()
+			local mason = require("mason")
+			local mason_tool_installer = require("mason-tool-installer")
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗"
+					}
+				}
+			})
+			mason_tool_installer.setup {
+
+				-- a list of all tools you want to ensure are installed upon
+				-- start; they should be the names Mason uses for each tool
+				ensure_installed = {
+
+					{"texlab", version="v4.3.0"},
+
+					"cmake-language-server",      --require: sudo apt install python3-venv
+					"json-lsp",			--require: nodejs
+					"clangd",
+					"lua-language-server",
+					"marksman",
+					"pyright",
+				},
+
+				auto_update = false,
+
+				run_on_start = true,
+
+				start_delay = 1000, -- 3 second delay
+
+				-- debounce_hours = 5,
+
+			}
+
+		end
+	}
 }
