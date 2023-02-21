5a50387eeda7a2bb13cd3146755d3d624e48c048
XiaotianXu <xxt990318@outlook.com>
2023-02-21 11:17:57 +0800

    noice lsp document roll keymaps

--- nvim/lua/user/plugins/ui.lua
+++ nvim/lua/user/plugins/ui.lua
@@ -293,6 +293,19 @@ return {
 					bottom_search = true,
 				}
 			})
+
+			vim.keymap.set({"n", "i", "s"}, "<c-j>", function()
+				if not require("noice.lsp").scroll(4) then
+					return "<c-f>"
+				end
+			end, { silent = true, expr = true })
+
+			vim.keymap.set({"n", "i", "s"}, "<c-k>", function()
+				if not require("noice.lsp").scroll(-4) then
+					return "<c-b>"
+				end
+			end, { silent = true, expr = true })
+
 		end,
 	},
 
