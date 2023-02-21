8252f0fb532d2f2141096e8a1580fe53dc22a7cb
XiaotianXu <xxt990318@outlook.com>
2023-02-21 14:02:40 +0800

    lsp highlight document under cursor

--- nvim/lua/user/plugins/lsp/handlers.lua
+++ nvim/lua/user/plugins/lsp/handlers.lua
@@ -50,17 +50,29 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
+  if client.server_capabilities.documentHighlightProvider then
+    -- vim.api.nvim_exec(
+    --   [[
+    --   augroup lsp_document_highlight
+    --     autocmd! * <buffer>
+    --     autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+    --     autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    --   augroup END
+    -- ]],
+    --   false
+    -- )
+		vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
+		vim.api.nvim_clear_autocmds({ buffer = bufnr, group = 'lsp_document_highlight', })
+		vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+			group = 'lsp_document_highlight',
+			buffer = bufnr,
+			callback = vim.lsp.buf.document_highlight,
+		})
+		vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+			group = 'lsp_document_highlight',
+			buffer = bufnr,
+			callback = vim.lsp.buf.clear_references,
+		})
   end
 end
 
@@ -115,9 +127,6 @@ end
 
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
-		client.server_capabilities.document_formatting = false
-  end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 	-- lsp_hover_diag_auto()
