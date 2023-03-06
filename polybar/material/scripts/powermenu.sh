cab8236e705cfb9f387aae517df96bdb70880792
XiaotianXu <xxt990318@outlook.com>
2023-03-06 13:21:38 +0800

    use gnome screen lock

--- polybar/material/scripts/powermenu.sh
+++ polybar/material/scripts/powermenu.sh
@@ -58,11 +58,12 @@ case $chosen in
         fi
         ;;
     $lock)
-		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
-		elif [[ -f /usr/bin/betterlockscreen ]]; then
-			betterlockscreen -l
-		fi
+		gnome-screensaver-command -l
+		# if [[ -f /usr/bin/i3lock ]]; then
+		# 	i3lock
+		# elif [[ -f /usr/bin/betterlockscreen ]]; then
+		# 	betterlockscreen -l
+		# fi
         ;;
     $suspend)
 		ans=$(confirm_exit &)
