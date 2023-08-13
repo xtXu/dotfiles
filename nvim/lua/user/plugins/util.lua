43c9d01b390baf5d17cdb5592c1a2f193faa1a18
XiaotianXu <xxt990318@outlook.com>
2023-08-14 01:56:06 +0800

    nvim: vimtex use skim in macos, and okular in others

--- nvim/lua/user/plugins/util.lua
+++ nvim/lua/user/plugins/util.lua
@@ -59,7 +59,14 @@ return {
 			]]
 
 			-- vim.g.vimtex_view_method = 'okular'
-			vim.g.vimtex_view_general_viewer = 'okular'
+			if vim.loop.os_uname().sysname == "Darwin" then
+				vim.g.vimtex_view_method = 'skim'
+				vim.g.vimtex_view_skim_reading_bar = 1
+				vim.g.vimtex_view_skim_sync = 1
+			else
+				vim.g.vimtex_view_general_viewer = 'okular'
+				vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+			end
 			vim.g.tex_flavor='latex'
 			-- vim.g.vimtex_compiler_latexmk_engines = {
 			--     _ = '-xelatex'
@@ -76,7 +83,6 @@ return {
 			-- vim.g.tex_comment_nospell = 1
 			vim.g.vimtex_quickfix_mode = 0
 			-- vim.g.vimtex_compiler_progname = 'nvr'
-			vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
 			-- vim.g.vimtex_view_general_options_latexmk = '--reuse-instance'
 			--
 			vim.cmd[[
