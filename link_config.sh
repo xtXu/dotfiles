12fd2749aa292de76d65761129332970da94e238
xuxiaotian <xiaotian.xu@zelostech.com>
2024-11-16 23:12:08 +0800

    20241116.2

--- link_config.sh
+++ link_config.sh
@@ -7,22 +7,22 @@ if [ ! -d "$HOME/.config" ]; then
 	echo "Creat .config"
 fi
 
-if [! -f "$HOME/.zshrc" ]; then
+if [ ! -f "$HOME/.zshrc" ]; then
 	ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 	echo "linking zsh config..."
 fi
 
-if [! -d "$HOME/.config/nvim" ]; then
+if [ ! -d "$HOME/.config/nvim" ]; then
 	ln -s $HOME/dotfiles/nvim_mini $HOME/.config/nvim
 	echo "linking nvim config..."
 fi
 
-if [! -f "$HOME/.config/starship.toml" ]; then
+if [ ! -f "$HOME/.config/starship.toml" ]; then
 	ln -s $HOME/dotfiles/starship/starship.toml $HOME/.config/starship.toml
 	echo "linking starship config..."
 fi
 
-if [! -d "$HOME/.config/wezterm" ]; then
+if [ ! -d "$HOME/.config/wezterm" ]; then
 	ln -s $HOME/dotfiles/wezterm $HOME/.config/wezterm
 	echo "linking wezterm config..."
 fi
